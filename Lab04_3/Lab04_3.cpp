c403a26aa1dd8e64cb9561bb73266b517f5a143d
Tylko-jedno <diogkek@gmail.com>
2024-09-22 23:35:27 +0300

    first master commit

---
+++ Lab04_3/Lab04_3.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    double x, xp, xk, dx, a, b, c, F;
+    cout << "xp = "; cin >> xp;
+    cout << "xk = "; cin >> xk;
+    cout << "dx = "; cin >> dx;
+    cout << "a = "; cin >> a;
+    cout << "b = "; cin >> b;
+    cout << "c = "; cin >> c;
+
+    cout << fixed;
+    cout << "----------------------" << endl;
+    cout << "|" << setw(7) << "x" << " |"
+        << setw(10) << "F" << " |" << endl;
+    cout << "----------------------" << endl;
+
+    x = xp;
+    while (x <= xk) {
+        if (x + 5 < 0 && c == 0)
+            F = 1/(a*x) - b;
+        else {
+            if (x + 5 > 0 && c != 0)
+                F = (x - a) / x;
+            else
+                F = (10*x)/(c - 4);
+        }
+        cout << "|" << setw(7) << setprecision(2) << x
+            << " |" << setw(10) << setprecision(3) << F
+            << " |" << endl;
+        x += dx;
+    }
+    cout << "----------------------" << endl;
+}
